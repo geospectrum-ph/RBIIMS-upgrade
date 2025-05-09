a956b9f317113fc48711480a1443d236ce435870
Geospectrum Marketing Services - IT Department <it.support@geospectrum.com.ph>
2025-05-09 13:03:24 +0800

    initial frontend

---
+++ frontend/src/layout/main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function Main() {
+  return (
+    <main className="main-content">
+      <h1>RBIIMS</h1>
+      <p>main content area.</p>
+    </main>
+  );
+}
+
+export default Main;
